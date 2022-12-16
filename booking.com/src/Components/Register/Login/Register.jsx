07405eaa195995889c259b0f77229701e3698faf
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-12-16 16:14:45 +0530

    Update Register.jsx

--- booking.com/src/Components/Register/Login/Register.jsx
+++ booking.com/src/Components/Register/Login/Register.jsx
@@ -1,40 +1,11 @@
-// import { Box, Button, Input } from '@chakra-ui/react'
-// import React from 'react'
-// import { useState } from 'react'
-
-// const Register = () => {
-//   let auth = JSON.parse(localStorage.getItem('auth')) || []
-//   const [email , setemail] = useState('')
-//   const [password , setpassword] = useState('')
-//   const handlelogin = () => {
-
-//     auth.push({email, password})
-//     localStorage.setItem('auth', JSON.stringify(auth))
-
-//   }
-//   return (
-//     <Box>
-//       <Input value={email} onChange={(e) => setemail(e.target.value)} />
-//       <p>jfycdut</p>
-//       <Input value={password} onChange={(e) => setpassword(e.target.value)} />
-//       <Button onClick={handlelogin}>Login</Button>
-//     </Box>
-//   )
-// }
-
-// export default Register
-
 import React from "react";
 import { useState } from "react";
-//import { useNavigate } from "react-router-dom"
 
 let DataArray = [];
 
 const Register = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  // const [data, setdata] = useState([]);
-  //const navigate = useNavigate();
 
   const handleRegister = () => {
     DataArray.push({ email, password });
