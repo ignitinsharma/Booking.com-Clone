43132af41b16772938d89027f1f0669bb1bb4c35
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-12-16 09:56:12 +0530

    Update AllRoutes.jsx

--- booking.com/src/Components/AllRoutes/AllRoutes.jsx
+++ booking.com/src/Components/AllRoutes/AllRoutes.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
 import Hotels from "../Hotels/Hotels";
-import Login from "../Login/Login";
-import NotFound from "../NotFound/NotFound";
-import SignUp from "../SignUp/SignUp";
 import SingleHotelPage from "../SingleHotelPage/SingleHotelPage";
+import NotFound from "../NotFound/NotFound";
+import Login from "../Register/Login/Login";
+import Register from "../Register/Login/Register";
 
 const AllRoutes = () => {
   return (
@@ -13,8 +13,8 @@ const AllRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/hotels" element={<Hotels />} />
       <Route path="/hotels/:id" element={<SingleHotelPage />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
