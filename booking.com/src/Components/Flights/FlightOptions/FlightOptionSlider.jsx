754d9c3d925beb0f8e3b3af68b86afb22e61010a
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-12-17 21:49:45 +0530

    added data

---
+++ booking.com/src/Components/Flights/FlightOptions/FlightOptionSlider.jsx
@@ -0,0 +1,43 @@
+import {
+  Box,
+  Slider,
+  SliderFilledTrack,
+  SliderMark,
+  SliderThumb,
+  SliderTrack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+function FlightOptionSlider() {
+  const [sliderValue, setSliderValue] = useState(27);
+
+  const labelStyles = {
+    mt: "2",
+    ml: "-2.5",
+    fontSize: "sm",
+  };
+
+  return (
+    <Box pt={6} pb={2}>
+      <Slider aria-label="slider-ex-6">
+        <SliderMark
+          value={sliderValue}
+          textAlign="center"
+          bg="blue.500"
+          color="white"
+          mt="-10"
+          ml="-5"
+          w="12"
+        >
+          {sliderValue}hr
+        </SliderMark>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+    </Box>
+  );
+}
+
+export default FlightOptionSlider;
