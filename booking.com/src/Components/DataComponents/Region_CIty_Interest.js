196fb9d7b76093a529c131a86057bb35b848412c
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-12-19 02:42:08 +0530

    Create Region_CIty_Interest.js

---
+++ booking.com/src/Components/DataComponents/Region_CIty_Interest.js
@@ -0,0 +1,405 @@
+const Region = [
+  {
+    name: "Isle of Wight",
+    properties: "993 properties",
+  },
+  {
+    name: "Tenerife",
+    properties: "9,601 properties",
+  },
+  {
+    name: "Hawaii",
+    properties: "5,516 properties",
+  },
+  {
+    name: "Texel",
+    properties: "413 properties",
+  },
+  {
+    name: "Mykonos",
+    properties: "1,452 properties",
+  },
+  {
+    name: "Bora Bora",
+    properties: "59 properties",
+  },
+  {
+    name: "Bali",
+    properties: "12,679 properties",
+  },
+  {
+    name: "Uttar Pradesh",
+    properties: "4,370 properties",
+  },
+  {
+    name: "Phuket Province",
+    properties: "5,494 properties",
+  },
+  {
+    name: "Cornwall",
+    properties: "5,275 properties",
+  },
+  {
+    name: "Ibiza",
+    properties: "1,640 properties",
+  },
+  {
+    name: "Santorini",
+    properties: "1,762 properties",
+  },
+  {
+    name: "Lake District",
+    properties: "2,403 properties",
+  },
+  {
+    name: "Bihar",
+    properties: "761 properties",
+  },
+  {
+    name: "Jersey",
+    properties: "89 properties",
+  },
+  {
+    name: "England",
+    properties: "73,969 properties",
+  },
+  {
+    name: "Zanzibar",
+    properties: "835 properties",
+  },
+  {
+    name: "Ras Al Khaimah",
+    properties: "113 properties",
+  },
+  {
+    name: "Guernsey",
+    properties: "61 properties",
+  },
+];
+
+const Cities = [
+  {
+    name: "Bangkok",
+    properties: "4,040 hotels",
+  },
+  {
+    name: "York",
+    properties: "830 hotels",
+  },
+  {
+    name: "Newcastel upon Tyne",
+    properties: "387 hotels",
+  },
+  {
+    name: "Manchester",
+    properties: "1,087 hotels",
+  },
+  {
+    name: "London",
+    properties: "14,522 hotels",
+  },
+  {
+    name: "Liverpool",
+    properties: "1,083 hotels",
+  },
+  {
+    name: "Leeds",
+    properties: "381 hotels",
+  },
+  {
+    name: "Glasgow",
+    properties: "915 hotels",
+  },
+  {
+    name: "Edinburgh",
+    properties: "3,344 hotels",
+  },
+  {
+    name: "Carbis Bay",
+    properties: "69 hotels",
+  },
+  {
+    name: "Bristol",
+    properties: "643 hotels",
+  },
+  {
+    name: "Brighton & Hove",
+    properties: "856 hotels",
+  },
+  {
+    name: "Bournemouth",
+    properties: "404 hotels",
+  },
+  {
+    name: "Blackpool",
+    properties: "896 hotels",
+  },
+  {
+    name: "Birmingham",
+    properties: "1,009 hotels",
+  },
+  {
+    name: "Stockholm",
+    properties: "445 hotels",
+  },
+  {
+    name: "Manila",
+    properties: "4,797 hotels",
+  },
+  {
+    name: "Kuala Lumpur",
+    properties: "4,224 hotels",
+  },
+  {
+    name: "Amsterdam",
+    properties: "1,913 hotels",
+  },
+  {
+    name: "Berlin",
+    properties: "1,759 hotels",
+  },
+  {
+    name: "Sydney",
+    properties: "3,621 hotels",
+  },
+  {
+    name: "Perth",
+    properties: "887 hotels",
+  },
+  {
+    name: "Melbourne",
+    properties: "2,831 hotels",
+  },
+  {
+    name: "Gold Coast",
+    properties: "1,574 hotels",
+  },
+  {
+    name: "Canberra",
+    properties: "184 hotels",
+  },
+  {
+    name: "Brisbane",
+    properties: "893 hotels",
+  },
+  {
+    name: "Adelaide",
+    properties: "416 hotels",
+  },
+  {
+    name: "Wellington",
+    properties: "164 hotels",
+  },
+  {
+    name: "Auckland",
+    properties: "1,683 hotels",
+  },
+  {
+    name: "Killarney",
+    properties: "263 hotels",
+  },
+  {
+    name: "Galway",
+    properties: "404 hotels",
+  },
+  {
+    name: "Dublin",
+    properties: "1,229 hotels",
+  },
+  {
+    name: "Cork",
+    properties: "138 hotels",
+  },
+  {
+    name: "Paris",
+    properties: "7,578 hotels",
+  },
+  {
+    name: "Hong Kong",
+    properties: "813 hotels",
+  },
+  {
+    name: "Doha",
+    properties: "160 hotels",
+  },
+  {
+    name: "Ras al Khaimah",
+    properties: "114 hotels",
+  },
+  {
+    name: "Dubai",
+    properties: "4,299 hotels",
+  },
+  {
+    name: "Fujairah",
+    properties: "22 hotels",
+  },
+  {
+    name: "Ajman",
+    properties: "81 hotels",
+  },
+  {
+    name: "Abu Dhabi",
+    properties: "169 hotels",
+  },
+  {
+    name: "Istanbul",
+    properties: "4,217 hotels",
+  },
+  {
+    name: "Seoul",
+    properties: "2,304 hotels",
+  },
+  {
+    name: "Vancouver",
+    properties: "627 hotels",
+  },
+  {
+    name: "Toronto",
+    properties: "2,034 hotels",
+  },
+  {
+    name: "Ottawa",
+    properties: "240 hotels",
+  },
+  {
+    name: "Niagara Falls",
+    properties: "253 hotels",
+  },
+  {
+    name: "Montreal",
+    properties: "1,118 hotels",
+  },
+  {
+    name: "Kelowna",
+    properties: "214 hotels",
+  },
+  {
+    name: "Jasper",
+    properties: "55 hotels",
+  },
+  {
+    name: "Edmonton",
+    properties: "274 hotels",
+  },
+  {
+    name: "Canmore",
+    properties: "149 hotels",
+  },
+  {
+    name: "Banff",
+    properties: "50 hotels",
+  },
+  {
+    name: "Barcelona",
+    properties: "3,319 hotels",
+  },
+  {
+    name: "Tokyo",
+    properties: "5,655 hotels",
+  },
+  {
+    name: "Rome",
+    properties: "14,102 hotels",
+  },
+  {
+    name: "Singapore",
+    properties: "808 hotels",
+  },
+  {
+    name: "Whistler",
+    properties: "332 hotels",
+  },
+  {
+    name: "Rotorua",
+    properties: "279 hotels",
+  },
+  {
+    name: "Queenstown",
+    properties: "823 hotels",
+  },
+];
+
+const Interest = [
+  {
+    name: "Kalahari Waterpark",
+    properties: "43 properties",
+  },
+  {
+    name: "Al Maha wildlife Reserve",
+    properties: "1 properties",
+  },
+  {
+    name: "River Walk",
+    properties: "671 properties",
+  },
+  {
+    name: "Burj Al Arab Tower",
+    properties: "4,299 properties",
+  },
+  {
+    name: "Niagara Falls State Park",
+    properties: "75 properties",
+  },
+  {
+    name: "Disneyland",
+    properties: "247 properties",
+  },
+  {
+    name: "Portmeirion",
+    properties: "9 properties",
+  },
+  {
+    name: "Heathrow Terminal 2",
+    properties: "",
+  },
+  {
+    name: "Wembley Stadium",
+    properties: "14,522 properties",
+  },
+  {
+    name: "State Park",
+    properties: "21 properties",
+  },
+  {
+    name: "Guadalupe River Tubing",
+    properties: "327 properties",
+  },
+  {
+    name: "Ocean City Boarddwalk",
+    properties: "768 properties",
+  },
+  {
+    name: "Eiffel Tower",
+    properties: "7,578 properties",
+  },
+  {
+    name: "Disneyland Paris",
+    properties: "7,578 properties",
+  },
+  {
+    name: "Times Square",
+    properties: "1,383 properties",
+  },
+  {
+    name: "The Shard",
+    properties: "14,522 properties",
+  },
+  {
+    name: "Universal Studios Orlando",
+    properties: "3,691 properties",
+  },
+  {
+    name: "O2 Arena",
+    properties: "14,522 properties",
+  },
+  {
+    name: "Comal River Tubing",
+    properties: "327 properties",
+  },
+  {
+    name: "Heathrow Terminal 5",
+    properties: "61 properties",
+  },
+];
+export { Region, Cities, Interest };
